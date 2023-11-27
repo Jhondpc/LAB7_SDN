dad0f9497c058577c6108d30770c2e39b587a2a1
Jhondpc <a20196137@pucp.edu.pe>
2023-11-26 19:13:51 -0500

    d

--- src/main/java/com/example/wssdn/WsSdnApplication.java
+++ src/main/java/com/example/wssdn/WsSdnApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackageClasses = {WsSdnApplication.class, com.example.wssdn.entity.BlackListIP.class})
+@EntityScan(basePackages = "com.example.wssdn")
 public class WsSdnApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(WsSdnApplication.class, args);
 	}
+
 }
